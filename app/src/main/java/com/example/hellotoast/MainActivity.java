6ae2f9b17cdb02d805c405efbaa406721f0a4262
vuong tran <vuong.thcodeforces194412@outlook.com.vn>
2022-04-09 12:09:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnToast = findViewById(R.id.button_toast);
+        Button btnCount = findViewById(R.id.button_count);
+        TextView mShowCount = findViewById(R.id.show_count);
+
+        btnToast.setOnClickListener(v -> showToast());
+        btnCount.setOnClickListener(v -> countUp(mShowCount));
+    }
+
+    private void showToast() {
+        Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void countUp(TextView mShowCount) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(String.valueOf(mCount));
+        }
+    }
+}
